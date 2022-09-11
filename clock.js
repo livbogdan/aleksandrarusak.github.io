8f94c5c28d8045fb7c9bd5bb811a83c237681b36
AleksandraRusak <rusakaleksandra94@gmail.com>
2022-09-11 12:02:12 +0200

    Update clock.js

--- clock.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    window.setInterval(function(){
-    var now = new Date();
-    var clock = document.getElementById("clock");
-    clock.innerHTML = now.toLocaleTimeString();
-    },1000);
-  };
